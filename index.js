255d251eef67bd5ffd0b1fab18f42fbb6d4d806c
Raghu-KV <raghunandanv19@gmail.com>
2023-04-23 19:02:22 +0530

    socket-be

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 4000;
+
+const server = app.listen(PORT, () =>
+  console.log(`working in the port:${PORT}`)
+);
+
+const FRONT_END_LINK = "http://localhost:3000";
+
+const io = new Server(server, {
+  cors: {
+    origin: FRONT_END_LINK,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected: ${socket.id}`);
+
+  socket.on("join_room", (roomNum) => {
+    socket.join(roomNum);
+    console.log(`user ${socket.id} has joined the room: ${roomNum}`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("recived_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user left");
+  });
+});
